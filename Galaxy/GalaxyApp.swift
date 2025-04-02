93a9cf83e01a2fd619954abdc1c380676f26059f
keshew <artyom.korotckov2017@yandex.ru>
2025-04-02 22:41:39 +0500

    Initial Commit

---
+++ Galaxy/GalaxyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalaxyApp.swift
+//  Galaxy
+//
+//  Created by Артём Коротков on 02.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct GalaxyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
